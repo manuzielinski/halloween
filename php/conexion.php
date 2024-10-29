bcc7aeed34d33af24afd5c07c6cd35bc1daa247e
Manuel Zielinski <107320815+manuzielinski@users.noreply.github.com>
2024-10-29 20:31:00 -0300

    halloween desafio

---
+++ php/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$db   = 'halloween';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Error en la conexión: ' . $e->getMessage();
+}
+?>
